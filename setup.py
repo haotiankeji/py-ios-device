c39a56e1eccbebcc369b769b9834e3c1458ccbb4
haotiankeji <18660268129@163.com>
2022-09-05 20:41:38 +0800

    Update setup.py
    修改打包信息

--- setup.py
+++ setup.py
@@ -11,11 +11,11 @@ REQUIREMENTS = [i.strip() for i in open("requirements.txt").readlines()]
 setup(name='py_ios_device',
       version=__version__,
       description='Get ios data and operate ios devices',
-      author='chenpeijie & liyachao',
-      author_email='cpjsf@163.com',
-      maintainer='chenpeijie & liyachao',
+      author='xiaoying',
+      author_email='774621928@163.com',
+      maintainer='xiaoying & chenpeijie & liyachao',
       maintainer_email='',
-      url='https://github.com/YueChen-C/py-ios-device',
+      url='https://github.com/haotiankeji/py-ios-device',
       packages=find_packages(),
       long_description=open('README.md').read(),
       long_description_content_type="text/markdown",
@@ -28,7 +28,7 @@ setup(name='py_ios_device',
       ],
       entry_points={
           'console_scripts':{
-              'pyidevice=ios_device.main:cli'
+              'antpy=ios_device.main:cli'
           }
       },
 )
